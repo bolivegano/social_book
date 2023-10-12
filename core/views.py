cc725087612ffc2625fb7c1d5051ae4c52b995d2
Bolivegano <bolivegano@proton.me>
2023-10-11 21:42:55 -0600

    up to 3:00:00 in video tutorial

--- core/views.py
+++ core/views.py
@@ -2,12 +2,64 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Post
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url='login')
 def index(request):
-  return render(request, 'index.html')
+  user_object = User.objects.get(username=request.user.username)
+  user_profile = Profile.objects.get(user=user_object)
+  
+  posts = Post.objects.all()
+  return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
+
+@login_required(login_url='login')
+def upload(request):
+  
+  if request.method == "POST":
+    user = request.user.username
+    image = request.FILES.get('image_upload')
+    caption = request.POST['caption']
+    
+    new_post = Post.objects.create(user=user, image=image, caption=caption)
+    new_post.save()
+    
+    return redirect('index')
+  else:
+    return redirect('index')
+
+@login_required(login_url='login')
+def settings(request):
+  user_profile = Profile.objects.get(user=request.user)
+  
+  if request.method == "POST":
+    
+    if request.FILES.get('image') == None:
+      image = user_profile.profile_img
+      bio = request.POST['bio']
+      location = request.POST['location']
+      
+      user_profile.profile_img = image
+      user_profile.bio = bio
+      user_profile.location = location
+      user_profile.save()
+      
+    if request.FILES.get('image') is not None:
+      image = request.FILES.get('image')
+      bio = request.POST['bio']
+      location = request.POST['location']
+      
+      user_profile.profile_img = image
+      user_profile.bio = bio
+      user_profile.location = location
+      user_profile.save()
+      
+    return redirect('settings')
+  
+  return render(request, 'settings.html', {'user_profile': user_profile})
 
 def signup(request):
   
@@ -29,12 +81,14 @@ def signup(request):
         user.save()
         
         #log user in and redirect to settings page.
+        user_login = auth.authenticate(username=username, password=password1)
+        auth.login(request, user_login)
         
         #create a profile object for the new user
         user_model = User.objects.get(username=username)
         new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
         new_profile.save()
-        return redirect('signup')
+        return redirect('settings')
         
     else:
       messages.info(request, "Passwords don't match")
@@ -59,8 +113,9 @@ def login(request):
       return redirect('login')
     
   else:
-    return render(request, reverse('login'))
-  
+    return render(request, 'login.html')
+
+@login_required(login_url='login')  
 def logout(request):
   auth.logout(request)  
-  return redirect(reverse('login'))
+  return redirect('login')
